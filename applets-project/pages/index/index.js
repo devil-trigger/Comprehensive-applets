354d8fd4eea0e86bdf817a3eb0cc21fd29d5374b
ZhengRunFeng <320975636@qq.com>
2021-06-10 02:04:54 +0800

    新增数据请求和页面排版

--- applets-project/pages/index/index.js
+++ applets-project/pages/index/index.js
@@ -3,27 +3,39 @@ Page({
     data: {
         searchBtn:`width:100%`,
         active: 0,//tab
-        WeekList:{},
-
+        NewFilm:[],//新片
+        Weekly:[],//口碑
+        UsFilm:[],//北美票房
     },
 // 生命周期函数--监听页面加载
     onLoad: function (options) {
-        this.getData('weekly').then(res=>{console.log(res)})
+        this.getData('new_movies').then(res=>{//新片
+            // console.log(res.data.subjects[0])
+            this.setData({
+                NewFilm:res.data.subjects
+            })
+        })
+        this.getData('weekly').then(res=>{//口碑
+            // console.log(res.data.subjects[0])
+            this.setData({
+                Weekly:res.data.subjects
+            })
+        })
+        this.getData('us_box').then(res=>{//口碑
+            // console.log(res.data.subjects[0])
+            this.setData({
+                UsFilm:res.data.subjects
+            })
+        })
     },
-    getData(parameter){//数据
+    getData(parameter){//豆瓣数据请求
         return new Promise((resolve,reject)=>{
             let datajson={
                 url: `http://localhost:2080/v2/movie/${parameter}`,
-                data: {
-                    apikey:'0b2bdeda43b5688921839c8ecb20399b'
-                },
+                data: { apikey:'0b2bdeda43b5688921839c8ecb20399b' },
                 header: {'content-type': 'json'},
-                success (res) {
-                    resolve(res)
-                },
-                fail(err){
-                    reject(err)
-                }
+                success (res) { resolve(res) },
+                fail(err){ reject(err)}
               }
             wx.request(datajson)
         })
