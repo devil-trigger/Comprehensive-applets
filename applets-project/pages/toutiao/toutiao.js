7ff4160a30ce4baeb3dee984c835b1b70fb258ab
The bee <1242847392@qq.com>
2021-06-10 11:56:51 +0800

    增加头条

---
+++ applets-project/pages/toutiao/toutiao.js
@@ -0,0 +1,79 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    dataList:[]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.getdataList()
+  },
+  getdataList(){
+    wx.request({
+      url: 'http://api.tianapi.com/topnews/index',
+      data:{
+        key:'869941cd56fe09e14b255d12467651bd',
+        num:10
+      },
+      success:res=>{
+        console.log(res.data.newslist)
+        this.setData({
+          dataList:res.data.newslist
+        })
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
